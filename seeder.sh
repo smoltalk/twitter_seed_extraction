6753cb5a620f0f74aa73eb7cae2d14c77c7ecf29
smoltalk <58809172+smoltalk@users.noreply.github.com>
2019-12-12 02:07:35 -0800

    Create seeder.sh

---
+++ seeder.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+while IFS= read -r line
+
+do
+
+# twint is not a stock Linux command. It's a 3rd party Python tool for Twitter research
+twint -u $line --user-full > $line.txt
+echo "Sleeping for 10 seconds"
+sleep 10
+
+# "user" is a separate file that is a simple list of usernames
+done < user
+
+
